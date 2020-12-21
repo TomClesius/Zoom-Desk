0cf685a75b1aca7756a7e9ae4cb85ec5b3801d8c
tomclesius <tomclesius@gmail.com>
2020-12-21 21:49:23 +0100

    layout changes

---
+++ data-ini.py
@@ -0,0 +1,9 @@
+import pickle
+import pathlib
+
+abspath = pathlib.Path("data.dat").absolute()
+def safe(array):
+    pickle.dump(array,open(str(abspath),"wb"))
+
+ini = [[],[],[]]
+safe(ini)
--- main.py
+++ main.py
@@ -13,13 +13,17 @@ root = Tk()
 btn = [] #creates list to store the buttons in
 x = [] #creates list to store the x buttons in
 
+
+SessionFrame = LabelFrame(root,padx=10,pady=10)
+
+
+
 # TODO safe in file
 ZoomPath = []
 
 def addZoomPath():
     filename = askopenfilename(initialdir="/", title="Select File", filetypes = ((".exe","*.exe"),("all","*.*"))) 
     ZoomPath.append(filename)
-
 
 ##############ZoomCode####################
 def zoomClass(meet_id, password):
@@ -41,7 +45,6 @@ def zoomClass(meet_id, password):
     pyautogui.press('enter',interval=0.2)
 
 
-
 abspath = pathlib.Path("data.dat").absolute()
 
 def safe(array):
@@ -53,9 +56,12 @@ def load():
 
 #TODO index out of range BUG
 def delete(index):
+    if(len(btn) == 1):
+        SessionFrame.pack_forget()
+
+
     btn[index].destroy()
     x[index].destroy()
-
     data = load()
     del data[0][index]
     del data[1][index]
@@ -65,11 +71,6 @@ def delete(index):
     safe(data)
     iniBtns()
 
-
-
-
-
-
 ##############Displayes all already safed Buttons####################
 def iniBtns():
     # data = [[name],[id],[pw]]
@@ -77,64 +78,71 @@ def iniBtns():
     print(data)
     for i in range(len(data[0])): #this says for *counter* in *however many elements there are in the list files*
         #the below line creates a button and stores it in an array we can call later, it will print the value of it's own text by referencing itself from the list that the buttons are stored in
-        btn.append(Button(root, text= data[0][i], command=lambda c=i: zoomClass(data[1][c],data[2][c])))
-        btn[i].pack()
+
+        btn.append(Button(SessionFrame, text= data[0][i],padx=25, command=lambda c=i: zoomClass(data[1][c],data[2][c])))
+        x.append(Button(SessionFrame, text="x",padx=5, command=lambda c=i: delete(c)))
         
-        x.append(Button(root, text="x", command=lambda c=i: delete(c)))
-        x[i].pack()
+        btn[i].grid(row=i,column=0)
+        x[i].grid(row=i,column=1)
         
+
         safe(data)
         
-
-
 ##############Add Button function####################
-def refreshBtns():
+def addBtns():
+    print(len(btn))
+    if(len(btn) == 0):
+        SessionFrame.pack(padx=10,pady=10,)
     data = load()
-    data[2].append(str(passwort_wert.get())) 
-    data[0].append(str(name_wert.get())) 
-    data[1].append(str(id_wert.get()))  
+    data[2].append(str(password_value.get())) 
+    data[1].append(str(name_value.get())) 
+    data[0].append(str(id_value.get()))  
 
-    btn.append(Button(root, text=data[0][-1], command=lambda c= len(data[0])-1: zoomClass(data[1][c],data[2][c])))
-    btn[-1].pack() #this packs the buttons
-
-    x.append(Button(root, text="x", command=lambda c= len(x)-1: delete(c)))
-    x[-1].pack()
-
+    btn.append(Button(SessionFrame, text=data[0][-1],padx=25, command=lambda c= len(data[0])-1: zoomClass(data[1][c],data[2][c])))
+    x.append(Button(SessionFrame, text="x",padx=5, command=lambda c= len(x)-1: delete(c)))
+   
+    btn[-1].grid(row=len(btn)+1,column=0) #this packs the buttons
+    x[-1].grid(row=len(x)+1,column=1)
+   
     safe(data)
 
-#add button
-Button(root, text = "Add",command= refreshBtns).pack()
-Button(root, text = "Zoom Path",command= addZoomPath).pack()
 
-#input name
-name_wert = StringVar()
-nameFeld = Entry(root, textvariable=name_wert)
-Label(root, text="Name").pack()
-nameFeld.pack()
-#input id
-id_wert = StringVar()
-idFeld = Entry(root, textvariable=id_wert)
-Label(root, text="Session-ID").pack()
-idFeld.pack()
-#input passwort
-passwort_wert = StringVar()
-passwortFeld = Entry(root, textvariable=passwort_wert)
-Label(root, text="Passwort").pack()
-passwortFeld.pack()
+DepFrame = LabelFrame(root,padx=10,pady=10)
+DepFrame.pack(padx=10,pady=10)
+FileBtn = Button(DepFrame, text = "Zoom Path" ,command= 0,width=10) ##TODO COMMAND # idea no button- automatic command when nothig saved
+FileBtn.pack()
+#--------------------------------------------------------------------
+AddFrame = LabelFrame(root,padx=10,pady=10)
+AddFrame.pack(padx=10,pady=10)
 
 
+#Entrys
+name_value = StringVar()
+id_value = StringVar()
+password_value = StringVar()
+
+nameFeld = Entry(AddFrame, textvariable=name_value)
+idFeld = Entry(AddFrame, textvariable=id_value)
+passwortFeld = Entry(AddFrame, textvariable=password_value)
+
+idFeld.grid(row=1,column=0,padx=5)
+nameFeld.grid(row=1,column=1,padx=5)
+passwortFeld.grid(row=1,column=2,padx=5)
+
+#Entry labels
+NameLabel = Label(AddFrame, text="Name")
+IdLabel = Label(AddFrame, text="Session-ID")
+PwLabel = Label(AddFrame, text="Password")
+
+NameLabel.grid(row=0,column=0)
+IdLabel.grid(row=0,column=1)
+PwLabel.grid(row=0,column=2)
+
+#Add button
+AddBtn = Button(AddFrame, text = "Save",command = addBtns,width=10,)
+AddBtn.grid(row=1,column=3,padx=10,sticky="ew")
+SessionFrame.pack(padx=10,pady=10,)
 
 iniBtns()
 
 root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
