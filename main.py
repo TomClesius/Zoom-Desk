6e6ec7c101b03fd1998c353c9bb9779ba17bb583
tomclesius <tomclesius@gmail.com>
2020-12-22 03:23:07 +0100

    v1

--- main.py
+++ main.py
@@ -9,24 +9,18 @@ import time
 import pyautogui
 
 
-root = Tk(className="zoom-desk")
+root = Tk(className="zoomDesk")
+root.eval('tk::PlaceWindow . center')
 
 btn = [] #creates list to store the buttons in
 x = [] #creates list to store the x buttons in
 
 
-#####bei scripstart Zoom öffnen
-#####if beitreten drücken erkannt pyautogui
-#####automatisch kennwort etc füllen
-
-
-
-
 SessionFrame = LabelFrame(root,padx=10,pady=10)
-
 
 abspath = pathlib.Path("data.dat").absolute()
 
+#--------------------------------------------------------------------
 def safe(array):
     pickle.dump(array,open(str(abspath),"wb"))
 
@@ -38,7 +32,7 @@ def load():
 def addZoomPath():
     data = load()
     if not data[3] or data[3][0] == '':
-        filename = askopenfilename(initialdir="/", title="Select File", filetypes = ((".exe","*.exe"),("all","*.*"))) 
+        filename = askopenfilename(initialdir= "/", title="Select Zoom.exe", filetypes = ((".exe","*.exe"),("all","*.*"))) 
         data[3].append(filename) 
         safe(data)
 
@@ -48,7 +42,7 @@ def waitfor(img):
         print("here")
 
 
-##############ZoomCode####################
+#--------------------------------------------------------------------
 def zoomClass(meet_id, password):
     data = load()
     
@@ -73,7 +67,7 @@ def zoomClass(meet_id, password):
     pyautogui.press('enter',interval=0.4)
 
 
-#TODO index out of range BUG
+#--------------------------------------------------------------------
 def delete(index):
     btn[index].destroy()
     x[index].destroy()
@@ -86,35 +80,31 @@ def delete(index):
     safe(data)
     iniBtns()
 
-##############Displayes all already safed Buttons####################
+#--------------------------------------------------------------------
 def iniBtns():
-    # data = [[name],[id],[pw]]
+    # data = [[name],[id],[pw],[path]]
     data = load()
     print(data)
-    for i in range(len(data[0])): #this says for *counter* in *however many elements there are in the list files*
-        #the below line creates a button and stores it in an array we can call later, it will print the value of it's own text by referencing itself from the list that the buttons are stored in
-
-        btn.append(Button(SessionFrame, text= data[0][i],padx=25, command=lambda c=i: zoomClass(data[1][c],data[2][c])))
+    for i in range(len(data[0])): 
+        btn.append(Button(SessionFrame, text= data[0][i],padx=25, command=lambda c=i: zoomClass(data[1][c],data[2][c]),width = 30))
         x.append(Button(SessionFrame, text="x",padx=5, command=lambda c=i: delete(c)))
         
-        btn[i].grid(row=i,column=0)
+        btn[i].grid(row=i,column=0,pady = 2)
         x[i].grid(row=i,column=1)
-        
-
         safe(data)
         
-##############Add Button function####################
+#--------------------------------------------------------------------
 def addBtns():
     print(len(btn))
     data = load()
     data[2].append(str(password_value.get())) 
-    data[1].append(str(name_value.get())) 
-    data[0].append(str(id_value.get()))  
+    data[0].append(str(name_value.get())) 
+    data[1].append(str(id_value.get()))  
 
-    btn.append(Button(SessionFrame, text=data[0][-1],padx=25, command=lambda c= len(data[0])-1: zoomClass(data[1][c],data[2][c])))
+    btn.append(Button(SessionFrame, text=data[0][-1],padx=25, command=lambda c= len(data[0])-1: zoomClass(data[1][c],data[2][c]),width = 30))
     x.append(Button(SessionFrame, text="x",padx=5, command=lambda c= len(x): delete(c)))
    
-    btn[-1].grid(row=len(btn)+1,column=0) #this packs the buttons
+    btn[-1].grid(row=len(btn)+1,column=0,pady = 2) #this packs the buttons
     x[-1].grid(row=len(x)+1,column=1)
    
     safe(data)
@@ -123,7 +113,6 @@ def addBtns():
 #--------------------------------------------------------------------
 AddFrame = LabelFrame(root,padx=10,pady=10)
 AddFrame.pack(padx=10,pady=10)
-
 
 #Entrys
 name_value = StringVar()
@@ -134,8 +123,8 @@ nameFeld = Entry(AddFrame, textvariable=name_value)
 idFeld = Entry(AddFrame, textvariable=id_value)
 passwortFeld = Entry(AddFrame, textvariable=password_value)
 
-idFeld.grid(row=1,column=0,padx=5)
-nameFeld.grid(row=1,column=1,padx=5)
+nameFeld.grid(row=1,column=0,padx=5)
+idFeld.grid(row=1,column=1,padx=5)
 passwortFeld.grid(row=1,column=2,padx=5)
 
 #Entry labels
